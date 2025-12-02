4b01f73637ee3306721579a3fd61e6b7609119a1
zzz <jianghao_zhang@163.com>
2025-12-02 10:46:38 +0800

    完善sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   nitro: {
     prerender: {
-      routes: ['/gonggao','/gonggao/11', '/about'],
+      routes: ["/gonggao", "/gonggao/*", "/about"],
     },
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@element-plus/nuxt"],
+  modules: ["@nuxt/content", "@element-plus/nuxt", "@nuxtjs/sitemap"],
+  site: {
+    url: "https://shitu.pages.dev",
+    hostname:"https://shitu.pages.dev",
+    name: "My Awesome Website",
+  },
 });
