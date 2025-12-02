cd729c8ee3ea2d56ac99b78fe72a4314569229c4
zzz <jianghao_zhang@163.com>
2025-12-02 10:23:42 +0800

    完善

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/gonggao','/gonggao/11', '/about'],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@element-plus/nuxt"],
